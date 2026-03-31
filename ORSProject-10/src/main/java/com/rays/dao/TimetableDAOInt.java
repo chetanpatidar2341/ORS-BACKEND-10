373184571c6a0fae3013d8d86e68c1108bb7b4dd
chetanpatidar31 <chetan50682@gmail.com>
2026-03-31 15:11:55 +0530

    backend

--- ORSProject-10/src/main/java/com/rays/dao/TimetableDAOInt.java
+++
@@ -1,8 +0,0 @@
-package com.rays.dao;
-
-import com.rays.common.BaseDAOInt;
-import com.rays.dto.TimetableDTO;
-
-public interface TimetableDAOInt extends BaseDAOInt<TimetableDTO>{
-
-}
--- ORSProject-10/src/main/java/com/rays/service/RoleServiceInt.java
+++
@@ -1,8 +0,0 @@
-package com.rays.service;
-
-import com.rays.common.BaseServiceInt;
-import com.rays.dto.RoleDTO;
-
-public interface RoleServiceInt extends BaseServiceInt<RoleDTO> {
-
-}
